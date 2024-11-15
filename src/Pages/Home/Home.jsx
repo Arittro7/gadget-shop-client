a1766bd70c2573cdb5e838783be239e0f05cd141
Arittro7 <arittro007@gmail.com>
2024-11-15 21:51:31 +0600

    Applied Container for the home component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,19 +8,22 @@ const Home = () => {
     <div>
       <Banner></Banner>
       
-      <div className="container mx-auto my-24">
+      <div className="container mx-auto">
+        
+      <div className="my-24">
         <h1 className="text-center text-3xl font-semibold mb-6">Featured Product</h1>
       <FeaturedProduct></FeaturedProduct>
       </div>
 
-      <div className="container mx-auto my-24">
+      <div className="my-24">
         <h1 className="text-center text-3xl font-semibold mb-6">User Review</h1>
       <UserReviews></UserReviews>
       </div>
 
-      <div className="container mx-auto my-24">
+      <div className="my-24">
         <h1 className="text-center text-3xl font-semibold mb-6">Frequently Asked Questions</h1>
       <Accordion></Accordion>
+      </div>
       </div>
       
     </div>
