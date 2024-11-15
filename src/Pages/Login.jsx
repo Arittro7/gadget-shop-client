651fd9950c5fd540ce534c0fdf1a6b482dd83fae
Arittro7 <arittro007@gmail.com>
2024-11-15 23:16:50 +0600

    Login page coded

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,59 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
-      <h1>Have Account? Login to Your account</h1>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login Now!</h1>
+            <p className="py-6">
+              Want`s to buy the latest gadget in the market? Login to your now
+              and get the latest updates on the trending gadgets.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+              <p className="mb-4 text-center text-sm ">
+                New here? Create an Account {}
+                <Link to="/register" className="text-blue-500 font-bold">
+                Register
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
