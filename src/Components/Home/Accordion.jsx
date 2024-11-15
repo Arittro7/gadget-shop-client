bf0637b9a70907135fb8b1a712a05eb2d18db826
Arittro7 <arittro007@gmail.com>
2024-11-15 21:46:03 +0600

    Accordion added and css applied

--- src/Components/Home/Accordion.jsx
+++ src/Components/Home/Accordion.jsx
@@ -1,6 +1,6 @@
 const Accordion = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-2 w-1/2 mx-auto">
       <div className="collapse collapse-arrow bg-base-200">
         <input type="radio" name="my-accordion-2" defaultChecked />
         <div className="collapse-title text-xl font-medium">
